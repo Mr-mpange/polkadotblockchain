1a4a45c27318bd6bcc89dc615cc7ed368a1cace8
Mr-mpange <kilindosaid771@gmail.com>
2025-11-09 12:39:02 +0300

    ..

--- polkadot-analytics/backend/src/index.js
+++ polkadot-analytics/backend/src/index.js
@@ -10,7 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 app.use(helmet());
 app.use(express.json());
 app.use(morgan('dev'));
